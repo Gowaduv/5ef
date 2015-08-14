5b4de902c396033084b296dfc4e6b27507b95dc1
KMSkelton <kmskelton@outlook.com>
2015-08-13 22:27:26 -0700

    front page works; preparing to remove hambureger menu elements

---
+++ JS/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: true
+  });
+
+
+  // All the jQuery things for the hamburger menu
+
+  // $(".cross").hide();
+  // $(".menu").hide();
+  // $(".hamburger").click(function() {
+  //   $(".menu").animate({
+  //     width: "toggle"
+  //   }, function() {
+  //     $(".hamburger").hide();
+  //     $(".cross").show();
+  //   });
+  // });
+  //
+  // $(".cross").click(function() {
+  //   $(".menu").animate({
+  //     width: "toggle"
+  //   }, function() {
+  //     $(".cross").hide();
+  //     $(".hamburger").show();
+  //   });
+  // });
+
+}); // from document.ready
