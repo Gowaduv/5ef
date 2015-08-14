ec9d86b6f82776502c5b736af665093f95108475
KMSkelton <kmskelton@outlook.com>
2015-08-14 16:32:54 -0700

    index page works; available has two examples - need to figure out PayPal API.

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,28 +3,4 @@ $(document).ready(function() {
     mode: 'fade',
     captions: true
   });
-
-
-  // All the jQuery things for the hamburger menu
-
-  // $(".cross").hide();
-  // $(".menu").hide();
-  // $(".hamburger").click(function() {
-  //   $(".menu").animate({
-  //     width: "toggle"
-  //   }, function() {
-  //     $(".hamburger").hide();
-  //     $(".cross").show();
-  //   });
-  // });
-  //
-  // $(".cross").click(function() {
-  //   $(".menu").animate({
-  //     width: "toggle"
-  //   }, function() {
-  //     $(".cross").hide();
-  //     $(".hamburger").show();
-  //   });
-  // });
-
 }); // from document.ready
